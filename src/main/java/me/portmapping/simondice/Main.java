9e48a85a46774177659cb18d8c362172cb115e57
portmappings <portmapping.net@gmail.com>
2024-09-12 19:43:59 -0700

    chore: Added BasicPreventionListener

--- src/main/java/me/portmapping/simondice/Main.java
+++ src/main/java/me/portmapping/simondice/Main.java
@@ -5,6 +5,7 @@ import me.portmapping.simondice.board.provider.SimonDiceScoreboard;
 import me.portmapping.simondice.board.Aether;
 import me.portmapping.simondice.commands.SimonDiceCommand;
 import me.portmapping.simondice.game.Game;
+import me.portmapping.simondice.listeners.BasicPreventionListener;
 import me.portmapping.simondice.listeners.EntityListener;
 import me.portmapping.simondice.listeners.PlayerListener;
 import me.portmapping.simondice.utils.file.FileConfig;
@@ -42,6 +43,7 @@ public final class Main extends JavaPlugin {
     protected void registerListeners(){
         getServer().getPluginManager().registerEvents(new PlayerListener(), this);
         getServer().getPluginManager().registerEvents(new EntityListener(), this);
+        getServer().getPluginManager().registerEvents(new BasicPreventionListener(), this);
     }
 
     @Override
